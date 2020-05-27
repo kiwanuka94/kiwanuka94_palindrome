0fbf0873ae582ce20d35f71a91f16f0952689a1a
Sam Kimball <samkimball4@gmail.com>
2020-05-26 22:11:03 -0400

    Finish working and refactored palindrome method

--- lib/kiwanuka94_palindrome.rb
+++ lib/kiwanuka94_palindrome.rb
@@ -1,6 +1,18 @@
 require "kiwanuka94_palindrome/version"
 
-module Kiwanuka94Palindrome
-  class Error < StandardError; end
-  # Your code goes here...
+
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
+
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "kiwanuka94_palindrome"
 
 require "minitest/autorun"
+require "minitest/reporters"
+Minitest::Reporters.use!
